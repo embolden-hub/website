6f92d67668a7965f17e0853b78885ba33c984323
embolden-hub <support@embolden-hub.org.uk>
2021-07-18 11:30:38 +0100

    add privacy policy page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <Link href="/privacy-policy">Privacy Policy </Link>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
---
+++ pages/privacy-policy.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function PrivacyPolicy(){
+    return (
+        <div className="container">
+          <Head>
+            <title>PrivacyPolicy</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+    
+          <div>
+              Some PrivacyPolicy here 
+          </div>
+        </div>
+      )
+}
