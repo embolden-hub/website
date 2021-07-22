cd34a95dca82da049e45a876fe17ea62c978b995
embolden-hub <support@embolden-hub.org.uk>
2021-07-22 15:37:32 +0100

    fix typo

--- pages/explore-options.js
+++ pages/explore-options.js
@@ -2,7 +2,7 @@ import Layout from '../components/layout';
 import MainSection from '../components/mainSection';
 
 export default function ExploreOptions(){
-    const tile = 'Explore Options';
+    const title = 'Explore Options';
     return Layout(MainSection(<>
         <p>At Embolden we have a range of books and other information that can help you reflect on your situation and think about your options. 
 We also have self-referral forms for Women’s Aid ready to be filled out, or you can access these online on Women’s Aid website under ‘Make a Referral’. 
