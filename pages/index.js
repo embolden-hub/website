48120132182269ef4669c2a3972635d27e195406
embolden-hub <support@embolden-hub.org.uk>
2021-07-22 15:52:11 +0100

    added a space

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home(){
             <h2>We Belive</h2>
             <p>At Embolden, we believe in emboldening women by providing a safe, discrete place within the community to explore options and make calls for help. We aim to empower through information, support and kindness.  
 Every woman regardless of race, ethnicity, class, religion, sexual orientation and ability deserves to feel safe and respected. </p>
-            <p>The links in the menu above provide useful information and advice to help with any situation you may find your self in.</p>
+            <p>The links in the menu above provide useful information and advice to help with any situation you may find your self in. </p>
         </div>
     </div>, 'Home', true)
 }
