0f8af8b5c58eb02181a608b6cfd02008e429c007
embolden-hub <support@embolden-hub.org.uk>
2021-07-25 18:44:46 +0100

    add password check to find us

--- pages/where-to-find-us.js
+++ pages/where-to-find-us.js
@@ -1,15 +1,51 @@
 import Layout from '../components/layout';
 import MainSection from '../components/mainSection';
 import Link from 'next/link';
+import { useState } from 'react';
+
+const hashCode = function(s) {
+    var h = 0, l = s.length, i = 0;
+    if ( l > 0 )
+      while (i < l)
+        h = (h << 5) - h + s.charCodeAt(i++) | 0;
+    return h;
+};
+
 
 export default function WhereToFindUs({passwordProvided,setPasswordProvided}){
+    const [pwdEntered, setPwdEntered] = useState(false);
+    const [password, setPassword] = useState(undefined);
+    
+    const check = () => password !== undefined ? setPwdEntered(hashCode(password) === 1516355) : null;
+    const handleKeyDown = (e)  => {
+        if (e.key === 'Enter') {check()}
+    }
+
+    const enterPassword  = () => {
+        return <div className="row">
+                    <div className='col-xl-12' style={{display:'flex', alignItems:'center', justifyContent:'center', flexDirection:'column'}}>
+                        <h2 style={{margin:'10px'}}>Password</h2>
+                        <input
+                        style={{margin:'10px'}} 
+                        type="password" 
+                        className="form-control" 
+                        id="Password1" 
+                        placeholder="Password" 
+                        onKeyDown={handleKeyDown}
+                        onChange={evt => setPassword(evt.target.value)}/>
+                        <a onClick={check} className="thm-btn">Submit</a>
+                    </div>
+              </div>
+    }
+
+
     const title = "Find Us";
-    return Layout(MainSection(<div className='col-xl-12'>
-        <div className='row'>
-            <div className='col-xl-12' style={{marginBottom:'20px'}}>
-                <h5> Embolden Support Hub is located at Winslow Community Library. The address is as follows:</h5>
+    return Layout(!pwdEntered ? enterPassword() : MainSection( <div className='col-xl-12'>
+            <div className='row'>
+                <div className='col-xl-12' style={{marginBottom:'20px'}}>
+                    <h5> Embolden Support Hub is located at Winslow Community Library. The address is as follows:</h5>
+                </div>
             </div>
-        </div>
         <div className='row'>
             <div className="col-xl-6">
                 <div style={{display:'flex', flexDirection:'column'}}>
@@ -36,9 +72,8 @@ export default function WhereToFindUs({passwordProvided,setPasswordProvided}){
                 <div style={{overflow:'hidden',height:'350px', width:'350px', margin:'5px 0px'}}>
                     <iframe style={{width:'350px', height:'350px'}} id="gmap_canvas" src="https://maps.google.com/maps?q=winslow%20library&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"/>
                 </div>
-               
             </div>
-        </div>
+        </div> 
     </div>, title), title, passwordProvided, setPasswordProvided)
 }
 
